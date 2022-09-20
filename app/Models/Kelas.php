985904ded1b6b9bfdaea3b6eb4f414cada38fe55
adrians-23 <adriansondang@gmail.com>
2022-09-20 08:07:02 +0700

    add tables

---
+++ app/Models/Kelas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+    use HasFactory;
+
+    //ini ditambahkan jika nama tabel kita tidak jamak (dalam bahasa inggris)
+    protected $table = 'barang';
+
+    //ini jika kita menggunakan Eloquent ORM
+    protected $guarded = [];
+}
